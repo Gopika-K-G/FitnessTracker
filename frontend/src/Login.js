7c1cedf37e2e0ec9c541b35e11893905721f08c5
Gopika_K_G <120329606+Gopika-K-G@users.noreply.github.com>
2024-11-08 09:36:11 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/api/login', {
+      const response = await axios.post('https://fitnesstracker-6y74.onrender.com/api/login', {
         email,
         password,
       });
